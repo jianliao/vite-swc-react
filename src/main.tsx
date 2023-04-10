9bd3dfb3ef55c268982593776d7e1b94cecba150
Jian Liao <jianliao@adobe.com>
2023-04-10 09:46:59 -0700

    Add a simple swc-react setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "@spectrum-web-components/theme/theme-light.js";
+import '@spectrum-web-components/theme/express/theme-light.js';
+import "@spectrum-web-components/theme/scale-medium.js";
+import '@spectrum-web-components/theme/express/scale-medium.js';
+import { Theme } from "@swc-react/theme";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme theme="express" scale="medium" color="light">
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
